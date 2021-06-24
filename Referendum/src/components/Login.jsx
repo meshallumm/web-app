02c542fcc18e7455531375cd419445392f1eb5da
Elad H <eladhtn@gmail.com>
2021-06-24 17:51:13 +0300

    errorMessage

--- Referendum/src/components/Login.jsx
+++ Referendum/src/components/Login.jsx
@@ -8,7 +8,7 @@ class Login extends Component {
       input: '',
       mobileNum: '',
       number: '',
-      errorMessage: document.getElementById('error').textContent,
+      errorMessage: document.getElementById('error'),
     };
   }
 
