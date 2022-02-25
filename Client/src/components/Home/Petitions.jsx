f22e0e987f7d4d31e77abcf872d7c757decaa1d1
aviran <avirandabush@gmail.com>
2022-02-25 14:35:57 +0200

    fix color

--- Client/src/components/Home/Petitions.jsx
+++ Client/src/components/Home/Petitions.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Petitions() {
   return (
-  <div style={{backgroundColor: 'black'}}>
+  <div style={{backgroundColor: 'blue'}}>
       Petitions in component
   </div>
   )
